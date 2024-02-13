2508be851f2fddb0f2cb0c4f6d3f7df7dba6779f
Calculator <jucse28.364@gmail.com>
2024-02-13 23:56:13 +0600

    Hall_Admin,Hall Model Created

--- Hall_Management/Hall_Admin/models.py
+++ Hall_Management/Hall_Admin/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from Provost.models import *
 # Create your models here.
-# class HallAdmin(models.Model):
-#     name=models.CharField(max_length=100,blank=True,null=True)
-#     email=models.EmailField(max_length=100,blank=True,null=True)
-#     username=models.CharField(max_length=100,blank=True,null=True)
-#     password=models.CharField(max_length=100,blank=True,null=True)
-
-# class Hall(models.Model):
-#     name=models.CharField(max_length=100,blank=True,null=True)
-#     hallAdmin=models.ForeignKey(HallAdmin,on_delete=models.CASCADE,null=True,blank=True)
-#     provost=models.ForeignKey(Provost,on_delete=models.CASCADE,null=True,blank=True)
-#     def __str__(self):
+class HallAdmin(models.Model):
+    adminId=models.IntegerField(default=0,null=True,blank=True)
+    name=models.CharField(max_length=100,blank=True,null=True)
+    email=models.EmailField(max_length=100,blank=True,null=True)
+    username=models.CharField(max_length=100,blank=True,null=True)
+    password=models.CharField(max_length=100,blank=True,null=True)
+    def __str__(self):
+        return str(self.name)+" - "+str(self.adminId)
+class Hall(models.Model):
+    hallId=models.IntegerField(default=0,null=True,blank=True)
+    name=models.CharField(max_length=100,blank=True,null=True)
+    hallAdmin=models.ForeignKey(HallAdmin,on_delete=models.CASCADE,null=True,blank=True)
+    provost=models.ForeignKey(Provost,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return str(self.name)+" - "+str(self.hallId)
