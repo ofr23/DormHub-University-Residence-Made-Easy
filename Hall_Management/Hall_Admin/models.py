d5873deb2257877dc74f76aaf23855c69059dde4
Calculator <jucse28.383@gmail.com>
2024-02-14 00:02:16 +0600

    Staff Model Created

--- Hall_Management/Hall_Admin/models.py
+++ Hall_Management/Hall_Admin/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from Provost.models import *
 # Create your models here.
+
+class Hall(models.Model):
+    hallId=models.IntegerField(default=0,null=True,blank=True)
+    name=models.CharField(max_length=100,blank=True,null=True)
+
+    def __str__(self):
+        return str(self.name)+" - "+str(self.hallId)
 class HallAdmin(models.Model):
     adminId=models.IntegerField(default=0,null=True,blank=True)
     name=models.CharField(max_length=100,blank=True,null=True)
     email=models.EmailField(max_length=100,blank=True,null=True)
     username=models.CharField(max_length=100,blank=True,null=True)
     password=models.CharField(max_length=100,blank=True,null=True)
+    hall=models.ForeignKey(Hall,on_delete=models.CASCADE,null=True)
     def __str__(self):
         return str(self.name)+" - "+str(self.adminId)
-class Hall(models.Model):
-    hallId=models.IntegerField(default=0,null=True,blank=True)
-    name=models.CharField(max_length=100,blank=True,null=True)
-    hallAdmin=models.ForeignKey(HallAdmin,on_delete=models.CASCADE,null=True,blank=True)
-    provost=models.ForeignKey(Provost,on_delete=models.CASCADE,null=True,blank=True)
-    def __str__(self):
-        return str(self.name)+" - "+str(self.hallId)
--- Hall_Management/Staff/models.py
+++ Hall_Management/Staff/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from Hall_Admin.models import *
 # Create your models here.
+class Staff(models.Model):
+    staffId=models.IntegerField(default=0,null=True,blank=True)
+    name=models.CharField(max_length=100,blank=True,null=True)
+    email=models.EmailField(max_length=100,blank=True,null=True)
+    username=models.CharField(max_length=100,blank=True,null=True)
+    password=models.CharField(max_length=100,blank=True,null=True)
+    hall=models.ForeignKey(Hall,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return str(self.name)+" - "+str(self.staffId)
