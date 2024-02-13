f3b1375ca244032b8a93089699f7dc8051231286
Calculator <afzallafza@gmail.com>
2024-02-14 00:19:26 +0600

    Student Model Created

--- Hall_Management/Student/models.py
+++ Hall_Management/Student/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from Hall_Admin.models import *
+# Create your models here.from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    residentId=models.IntegerField(default=0,null=True,blank=True)
+    name=models.CharField(max_length=100,blank=True,null=True)
+    email=models.EmailField(max_length=100,blank=True,null=True)
+    username=models.CharField(max_length=100,blank=True,null=True)
+    password=models.CharField(max_length=100,blank=True,null=True)
+    hall=models.ForeignKey(Hall,on_delete=models.CASCADE,null=True)
+    def __str__(self):
+        return str(self.name)+" - "+str(self.studentId)
