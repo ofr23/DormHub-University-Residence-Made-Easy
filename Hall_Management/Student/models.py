3fbf8027c903f3ca8fba1d095fc4e5c63dd27c4a
Calculator <shshagor979@gmail.com>
2024-02-14 21:49:12 +0600

    model updated

--- Hall_Management/Student/models.py
+++ Hall_Management/Student/models.py
@@ -6,6 +6,7 @@ class Session(models.Model):
     csvFile=models.FileField(upload_to='csvs/',null=True,blank=True)
     def __str__(self):
         return str(self.session)
+    
 class Student(models.Model):
     residentId=models.IntegerField(default=0,null=True,blank=True)
     name=models.CharField(max_length=100,blank=True,null=True)
