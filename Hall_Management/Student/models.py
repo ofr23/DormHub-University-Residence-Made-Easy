14d22ce705adaefdb9b707ee2e94475f5e494dc9
Calculator <afzallafza@gmail.com>
2024-02-14 20:21:00 +0600

    Session Model added to Student APp

--- Hall_Management/Student/models.py
+++ Hall_Management/Student/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from Hall_Admin.models import *
 # Create your models here.from django.db import models
-
+class Session(models.Model):
+    session=models.IntegerField(default=0,null=True,blank=True)
+    def __str__(self):
+        return str(self.session)
 class Student(models.Model):
     residentId=models.IntegerField(default=0,null=True,blank=True)
     name=models.CharField(max_length=100,blank=True,null=True)
@@ -9,6 +12,6 @@ class Student(models.Model):
     username=models.CharField(max_length=100,blank=True,null=True)
     password=models.CharField(max_length=100,blank=True,null=True)
     hall=models.ForeignKey(Hall,on_delete=models.CASCADE,null=True)
-    session=models.IntegerField(default=0,null=True,blank=True)
+    session=models.ForeignKey(Session,on_delete=models.CASCADE,null=True)
     def __str__(self):
         return str(self.name)+" - "+str(self.studentId)
