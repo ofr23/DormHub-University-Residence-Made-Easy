ec39bbba1eceaaeff67fd72a6727f702d739340f
Calculator <afzallafza@gmail.com>
2024-02-15 00:02:08 +0600

    Authentication App Implemented

--- Hall_Management/Authentication/urls.py
+++ Hall_Management/Authentication/urls.py
@@ -6,7 +6,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
-    path('',views.logIn,name='logIn')
+    path('',views.logIn,name='logIn'),
+    path('register/',views.register,name='register'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Hall_Management/Authentication/views.py
+++ Hall_Management/Authentication/views.py
@@ -29,4 +29,54 @@ import codecs
 from openpyxl import load_workbook
 
 def logIn(request):
+    if 'login' in request.POST:
+        if request.POST.get('type')=='1':
+            user=Provost.objects.get(email=request.POST.get('email'))
+            login(request,authenticate(request,username=user.username,password=user.password))
+            redirectUrl="/provost"
+            return redirect(redirectUrl)
+        if request.POST.get('type')=='1':
+            user=HallAdmin.objects.get(email=request.POST.get('email'))
+            login(request,authenticate(request,username=user.username,password=user.password))
+            redirectUrl="/hallAdmin/"
+            return redirect(redirectUrl)
+        if request.POST.get('type')=='1':
+            login(request,authenticate(request,username=user.username,password=user.password))
+            redirectUrl="/aadmin/"+str(user.shopId)
+            return redirect(redirectUrl)
+        if request.POST.get('type')=='1':
+            login(request,authenticate(request,username=user.username,password=user.password))
+            redirectUrl="/aadmin/"+str(user.shopId)
+            return redirect(redirectUrl)
     return render(request,'logIn.html')
+def register(request):
+    if 'register' in request.POST:
+        if request.POST.get('type')=='1':
+            newUser=Provost.objects.get(email=request.POST.get('email'))
+            newUser.username=request.POST.get('username')
+            newUser.password=request.POST.get('password')
+            newUser.save()
+            user=User.objects.create_user(username=newUser.username,email=newUser.email,password=newUser.password)
+            return redirect('/authentication')
+        if request.POST.get('type')=='2':
+            newUser=HallAdmin.objects.get(email=request.POST.get('email'))
+            newUser.username=request.POST.get('username')
+            newUser.password=request.POST.get('password')
+            newUser.save()
+            user=User.objects.create_user(username=newUser.username,email=newUser.email,password=newUser.password)
+            return redirect('/authentication')
+        if request.POST.get('type')=='3':
+            newUser=Student.objects.get(email=request.POST.get('email'))
+            newUser.username=request.POST.get('username')
+            newUser.password=request.POST.get('password')
+            newUser.save()
+            user=User.objects.create_user(username=newUser.username,email=newUser.email,password=newUser.password)
+            return redirect('/authentication')
+        if request.POST.get('type')=='4':
+            newUser=Provost.objects.get(email=request.POST.get('email'))
+            newUser.username=request.POST.get('username')
+            newUser.password=request.POST.get('password')
+            newUser.save()
+            user=User.objects.create_user(username=newUser.username,email=newUser.email,password=newUser.password)
+            return redirect('/authentication')   
+    return render(request,'register.html')
