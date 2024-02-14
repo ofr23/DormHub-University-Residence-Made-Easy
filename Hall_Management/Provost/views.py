95f421d2da2831ce7ed8dc07efa97a4cb6310e9d
Calculator <nafizfouad@gmail.com>
2024-02-15 04:08:06 +0600

    Students Added to Hall

--- Hall_Management/Provost/views.py
+++ Hall_Management/Provost/views.py
@@ -34,11 +34,21 @@ def addStudent(request):
     provost=Provost.objects.get(email=request.user.email)
     hall=Hall.objects.get(provost=provost)
     if 'add' in request.POST:
-        csv=Session.objects.get(session=request.POST.get('session')).csvFile
+        session=Session.objects.get(session=int(request.POST.get('add')))
+        csv=session.csvFile
         wb = load_workbook(csv)
         ws = wb.active
         for row in ws.iter_rows(min_row=2, values_only=True):
-            print(row)
+            if int(row[4])==hall.hallId:
+                newStudent=Student(
+                    studentId=int(row[0]),
+                    session=session,
+                    name=row[1],
+                    hall=hall,
+                    email=row[2],
+                )
+                newStudent.save()
+        return redirect('/provost')
     queryDict={}
     for o in sessions:
         session=Session.objects.get(session=o.session)
@@ -50,4 +60,4 @@ def addStudent(request):
     context={
         'queryDict':queryDict
     }
-    return render(request,'addStudent.html',context)
+    return render(request,'allStudent.html',context)
--- Hall_Management/Student/models.py
+++ Hall_Management/Student/models.py
@@ -14,7 +14,7 @@ class Student(models.Model):
     username=models.CharField(max_length=100,blank=True,null=True)
     password=models.CharField(max_length=100,blank=True,null=True)
     hall=models.ForeignKey(Hall,on_delete=models.CASCADE,null=True)
-    session=models.IntegerField(default=0,null=True,blank=True)
+    session=models.ForeignKey(Session,on_delete=models.CASCADE,null=True,blank=True)
     def __str__(self):
         return str(self.name)+" - "+str(self.studentId)
     
