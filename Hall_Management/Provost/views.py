7a4c2b41a1466d975faf0163419f8a9b42a81e74
Calculator <nafizfouad@gmail.com>
2024-02-26 22:53:07 +0600

    Generated documentation using Sphinx

--- Hall_Management/Provost/views.py
+++ Hall_Management/Provost/views.py
@@ -1,11 +1,31 @@
-from Varsity_Admin.models import Hall, Room  # Import Hall and Room models
+"""
+Django views Configuration Documentation
+
+This module defines various controller files
+"""
 from Student.models import *  # Import Student and Session models
+from Varsity_Admin.models import *  # Import Hall and Room models
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from openpyxl import load_workbook
+
 from .models import *
 
+
 def provost(request):
+    """
+    View function to handle the provost view.
+
+    This function retrieves the provost object based on the logged-in user and the associated hall.
+    It then gets all rooms in the hall and creates a dictionary to store room availability information.
+    The function processes allocation, adding student to room, and removing student from room form submissions.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object rendering the provost template.
+    """
     # Get the provost object based on the logged-in user
     provost = Provost.objects.get(email=request.user.email)
     # Get the hall associated with the provost
@@ -24,7 +44,7 @@ def provost(request):
         available = room.capacity - len(Student.objects.filter(hall=hall, room=ro))
         queryDict[int(room.roomId / 100)].append((room, available))
 
-        # Process allocation form submission
+    # Process allocation form submission
     if 'allocate' in request.POST:
         room = Room.objects.get(roomId=int(request.POST.get('allocate')), hall=hall)
         residents = Student.objects.filter(room=room, hall=hall)
@@ -33,7 +53,7 @@ def provost(request):
         messages.success(request, {'available': available, 'room': room.roomId, 'residents': residents,
                                    'notAllocated': notAllocated}, extra_tags='allocation')
 
-        # Process adding student to room form submission
+    # Process adding student to room form submission
     if 'add' in request.POST:
         student = Student.objects.get(studentId=request.POST.get('select'))
         room = Room.objects.get(hall=hall, roomId=int(request.POST.get('room')))
@@ -56,6 +76,17 @@ def provost(request):
 
 
 def addStudent(request):
+    """
+    View function to handle adding students.
+
+    This function retrieves all sessions and processes form submission to add students.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object rendering the allStudent template.
+    """
     # Get all sessions
     sessions = Session.objects.all()
     provost = Provost.objects.get(email=request.user.email)
@@ -94,23 +125,36 @@ def addStudent(request):
         'queryDict': queryDict
     }
     return render(request, 'allStudent.html', context)
+
+
 def swapRequests(request):
-    provost=Provost.objects.get(email=request.user.email)
-    hall=Hall.objects.get(provost=provost)
-    requests=SwapRequest.objects.filter(hall=hall,status=0)
+    """
+    View function to handle swap requests.
+
+    This function retrieves swap requests associated with the provost's hall.
+    It processes form submissions to accept or reject swap requests.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object rendering the swapRequests template.
+    """
+    provost = Provost.objects.get(email=request.user.email)
+    hall = Hall.objects.get(provost=provost)
+    requests = SwapRequest.objects.filter(hall=hall, status=0)
     if 'accept' in request.POST:
-        req=SwapRequest.objects.get(hall=hall,student=
-                                      Student.objects.get(studentId=request.POST.get('accept')))
-        req.status=1
+        req = SwapRequest.objects.get(hall=hall, student=Student.objects.get(studentId=request.POST.get('accept')))
+        req.status = 1
         req.save()
         return redirect('/provost/swapRequests')
     if 'reject' in request.POST:
-        req=SwapRequest.objects.get(hall=hall,student=
-                                      Student.objects.get(studentId=request.POST.get('reject')))
-        req.status=2
+        req = SwapRequest.objects.get(hall=hall, student=Student.objects.get(studentId=request.POST.get('reject')))
+        req.status = 2
         req.save()
         return redirect('/provost/swapRequests')
-    context={
-        'requests':requests
+
+    context = {
+        'requests': requests
     }
-    return render(request,'swapRequests.html',context)
+    return render(request, 'swapRequests.html', context)
