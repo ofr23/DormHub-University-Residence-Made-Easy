41c3580426144b9e3921a6d254e772d5207f5eea
Shagor789 <152636436+Shagor789@users.noreply.github.com>
2024-02-27 00:57:59 +0600

    updated documentation

--- Hall_Management/Provost/models.py
+++ Hall_Management/Provost/models.py
@@ -11,6 +11,7 @@ class Provost(models.Model):
         username (str): The username of the provost.
         password (str): The password of the provost.
     """
+    
     provostId = models.IntegerField(default=1, null=True)  # Field for Provost ID
     name = models.CharField(max_length=100, blank=True, null=True)  # Field for Provost name
     email = models.EmailField(unique=True, max_length=100)  # Field for Provost email (unique)
